a8aa58a66cc7c0ab2f8be770fe705335c593c967
roshan-kmr <roshankumar.tarikhet@gmail.com>
2026-09-08 09:02:22 +0530

    Optimize mobile routine tables

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'workout-companion-v16';
+const CACHE_NAME = 'workout-companion-v19';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
